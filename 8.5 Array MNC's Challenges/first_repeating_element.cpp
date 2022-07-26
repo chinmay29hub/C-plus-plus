7891654ab7dbb83497b58b2bb211e65ec0f597ff
chinmay29hub <chinmaysonawane57@gmail.com>
2022-07-26 14:15:55 +0530

    updates

--- 8.5 Array MNC's Challenges/first_repeating_element.cpp
+++ 8.5 Array MNC's Challenges/first_repeating_element.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<climits>
 using namespace std;
 
 int main () {
@@ -9,6 +10,32 @@ int main () {
         cin>>array[i];
     }
 
+    const int N = 1e6 + 2;
+    int idx[N];
 
+    for (int i = 0; i<N; i++) {
+        idx[i] = -1;
+    }
+
+    int minidx = INT_MAX;
+
+    for (int i = 0; i<n; i++) {
+        if (idx[array[i]] != -1) {
+            minidx = min(minidx, idx[array[i]]);
+        }
+        else {
+            idx[array[i]] = i;
+        }
+    }
+
+    
+
+    if (minidx == INT_MAX) {
+        cout<<"-1"<<endl;
+    }
+    else {
+        cout<<minidx + 1<<endl;
+    }
+    return 0;
     
 }
