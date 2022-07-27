54139c73a50c0a04d611fe41d254e2fd2432ef6d
chinmay29hub <chinmaysonawane57@gmail.com>
2022-07-27 11:07:23 +0530

    character array

--- 10 Character Arrays/largest_word_in_sentence.cpp
+++ 10 Character Arrays/largest_word_in_sentence.cpp
@@ -5,15 +5,40 @@ int main () {
     int n;
 
     cin>>n;
-    // cin.ignore();
+    cin.ignore();
 
     char array[n+1];
 
     cin.getline(array, n);
     cin.ignore();
+
+    // cout<<array[0];
     
 
+    int temp = 0;
+    int newlen = 0;
     int i = 0;
+
+    // int st = 0;
+    // int end = 0;
+    while (array[i] != '\0') {
+        if (array[i] != ' ') {
+            newlen++;
+        } else {
+            temp = max(temp, newlen);
+            newlen = 0;
+        }
+
+        i++;
+    }
+    int res = max(temp, newlen);
+    cout<<res<<endl;
+
+    // cout<<s<<endl;
+
+    // for (int j = st; j<= st + res; j++) {
+    //     cout<<array[j];
+    // }
 
 
     return 0;
