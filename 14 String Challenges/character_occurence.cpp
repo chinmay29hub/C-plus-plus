2bfd4c5f016648901f8635b09590349ea2abd6b2
chinmay29hub <chinmaysonawane57@gmail.com>
2022-07-30 09:48:57 +0530

    string challenges

--- 14 String Challenges/character_occurence.cpp
+++ 14 String Challenges/character_occurence.cpp
@@ -9,16 +9,42 @@ int main () {
     string s;
     getline(cin, s);
 
-    int freq[26];
-
-    for (int i = 0; i<26; i++) {
-        freq[i] = 0;
-    }
+    int freq[26] = {0};
 
     for (int i = 0; i<s.size(); i++) {
-        freq[s[i] - 'a']
+        freq[s[i] - 'a']++;
+    }
+    int maxx = 0;    
+    char c = 'a';
+    for (int i = 0; i<26; i++) {
+        if (freq[i] > maxx) {
+            maxx = freq[i];
+            c = c + i;
+        }
     }
 
+    cout<<c<<" "<<maxx<<endl;
+
+
+    // below algorithm has more complexity
+    // int maxx = 0;
+    // char c;
+
+    // for (int i = 0; i<s.size(); i++) {
+    //     int count = 0;
+    //     for(int j = i; j<s.size(); j++) {
+    //         if (s[i] == s[j]) {
+    //             count++;
+    //             // maxx =max(maxx, count);
+    //         }
+    //     }
+    //     if (maxx < count) {
+    //         maxx = count;
+    //         c = s[i];
+    //     }
+        
+    // }
+    // cout<<maxx<<" "<<c<<endl;
     return 0;
 
 
