ca82efad9e7d25c9493e96e456003aa7e1068697
chinmay29hub <chinmaysonawane57@gmail.com>
2025-01-13 23:10:05 +0530

    OOPS

--- oops/oops.cpp
+++ oops/oops.cpp
@@ -171,6 +171,165 @@ public:
     }
 };
 
+class TeacherM
+{
+public:
+    string subject;
+    double salary;
+
+    TeacherM(string subject, double salary)
+    {
+        this->subject = subject;
+        this->salary = salary;
+    }
+};
+
+class StudentM
+{
+public:
+    string name;
+    int rollNo;
+
+    StudentM(string name, int rollNo)
+    {
+        this->name = name;
+        this->rollNo = rollNo;
+    }
+};
+
+class TA : public StudentM, public TeacherM
+{
+public:
+    TA(string name, int rollNo, string subject, double salary) : StudentM(name, rollNo), TeacherM(subject, salary)
+    {
+    }
+
+    void getInfo()
+    {
+        cout << "name: " << name << endl;
+        cout << "rollNo: " << rollNo << endl;
+        cout << "subject: " << subject << endl;
+        cout << "salary: " << salary << endl;
+    }
+
+    void nameOperation()
+    {
+        cout << "name: " << name << endl;
+    }
+
+    void nameOperation(string name)
+    {
+        this->name = name;
+        cout << "new name: " << name << endl;
+    }
+};
+
+class ParentA
+{
+public:
+    void getInfo()
+    {
+        cout << "Parent Class!" << endl;
+    }
+
+    virtual void hello()
+    {
+        cout << "hello from parent" << endl;
+    }
+};
+
+class ChildA : public ParentA
+{
+public:
+    void getInfo()
+    {
+        cout << "Child Class!" << endl;
+    }
+
+    void hello()
+    {
+        cout << "hello from child" << endl;
+    }
+};
+
+class Shape // automatically abstract class
+{
+    virtual void draw() = 0; // template (pure virtual function)
+};
+
+class Circle : public Shape
+{
+public:
+    void draw()
+    {
+        cout << "drawing a circle" << endl;
+    }
+};
+
+void fun()
+{
+    static int x = 5;
+    cout << "x: " << x << endl;
+    x++;
+}
+
+class A
+{
+public:
+    int x;
+
+    A()
+    {
+    }
+
+    A(int x)
+    {
+        this->x = x;
+    }
+
+    void incX()
+    {
+        x++;
+    }
+
+    void getInfo()
+    {
+        cout << "x: " << x << endl;
+    }
+};
+
+class ABC
+{
+public:
+    ABC()
+    {
+        cout << "constructor" << endl;
+    }
+
+    ~ABC()
+    {
+        cout << "destructor" << endl;
+    }
+};
+
+class staticTest
+{
+public:
+    static int count;
+
+    void inc()
+    {
+        count++;
+    }
+
+    void getInfo()
+    {
+        cout << "count: " << count << endl;
+    }
+};
+
+int staticTest::count = 0;
+
 int main()
 {
     Teacher t1("John", "C.S", "Maths", 23232);
@@ -198,8 +357,41 @@ int main()
     // CollegeStudent s1("Walter W.", 21, 23);
     GradStudent gs1("Walter W.", 21, 23, "Artificial Intelligence");
 
-    gs1.getInfo();
+    // gs1.getInfo();
 
+    TA ta1("John Wick", 23, "Maths", 343434);
+    // ta1.getInfo();
+    // ta1.nameOperation();
+    // ta1.nameOperation("Iron Man");
+    ChildA c1;
+    // c1.getInfo();
+    // c1.hello();
+    ParentA p1;
+    // p1.getInfo();
+    // p1.hello();
+    Circle s1;
+    // s1.draw();
+    // fun();
+    // fun();
+    // fun();
+    A inc1(5);
+    inc1.incX();
+    // inc1.getInfo();
+    inc1.incX();
+    // inc1.getInfo();
+    if (true)
+    {
+        static ABC aa1;
+    }
+    cout << "End of Main Func" << endl;
+
+    staticTest u1;
+    u1.count = 5;
+    u1.getInfo();
+    u1.inc();
+    u1.getInfo();
+    staticTest u2;
+    u2.getInfo();
     return 0;
 }
 
@@ -259,4 +451,52 @@ When properties & member functions of base class are passed on to the derived cl
 | Protected   | Private             | Protected           | Protected           |
 | Public      | Private             | Protected           | Public              |
 +-------------+---------------------+---------------------+---------------------+
+ */
+
+/*
+Inheritance Types:
+
+1) Single Inheritance
+2) Multi-level Inheritance
+3) Multiple Inheritance
+4) Hierarchical Inheritance
+5) Hybrid Inheritance
+ */
+
+/*
+Polymorphism
+
+Polymorphism is the ability of objects to take on different forms or behave in different
+ways depending on the context in which they are used.
+
+. Compile Time Polymorphism - eg: Constructor Overloading, Function Overloading
+
+. Run Time Polymorphism - eg: Function Overriding (Inheritance), Virtual Functions
+ */
+
+/*
+Abstraction
+Hiding all unnecessary details & showing only the important parts
+
+- Access Modifiers
+
+- using Abstract Classes
+
+. Abstract classes are used to provide a base class from which other classes can be derived.
+
+. They cannot be instantiated and are meant to be inherited.
+
+. Abstract classes are typically used to define an interface for derived classes.
+ */
+
+/*
+Static Keyword
+
+· Static Variables
+
+Variables declared as static in a function are created & initialised once for the lifetime of the
+program. //in Function
+
+Static variables in a class are created & initialised once. They are shared by all the
+objects of the class. //in Class
  */
